2bd302b5217438cdd61e2fa44fbf6245cfda2543
616164124@qq.com <616164124@qq.com>
2022-03-12 22:11:51 +0800

    Initial commit

---
+++ src/com/learn/test/Test.java
@@ -0,0 +1,13 @@
+package com.learn.test;
+
+import java.util.HashMap;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("11231");
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("1", "hua");
+        hashMap.put("2", "hua");
+
+    }
+}
